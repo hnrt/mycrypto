4f14d8a0b06b02cee355d581a1ddd2c3fa769edb
Hideaki Narita <naritahideaki2010@docomo.ne.jp>
2026-02-22 13:15:29 +0900

    fix minor in rebuild bat.

--- wincli/REBUILDALL.bat
+++ wincli/REBUILDALL.bat
@@ -1,4 +1,6 @@
-SETLOCAL
-set devenv=C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\devenv.com
-"%devenv%" .\wincli.vcxproj /rebuild "Debug|x64" || EXIT /B 1
-"%devenv%" .\wincli.vcxproj /rebuild "Release|x64" || EXIT /B 1
+@SETLOCAL
+@FOR /F "delims=" %%A IN ("%0") DO @SET dir1=%%~dpsA
+@SET vcxproj=%dir1%wincli.vcxproj
+@SET devenv=%ProgramFiles%\Microsoft Visual Studio\18\Community\Common7\IDE\devenv.com
+"%devenv%" %vcxproj% /rebuild "Debug|x64" || EXIT /B 1
+"%devenv%" %vcxproj% /rebuild "Release|x64" || EXIT /B 1
